7cdbc7cecbf76fd4df837ae11ed6a2649b10ff28
Nick Brown <nicholas.g.brown@gmail.com>
2016-04-01 21:12:18 -0400

    Added writeToBoard function to writes either X or O

--- js/app.js
+++ js/app.js
@@ -1,5 +1,24 @@
 $(document).ready(function() {
+    // Variables
+    var humanPlayer;
+    var computerPlayer;
+    var gameBoard = [ null, null, null, null, null, null, null, null, null ];
+    // Functions
+    // writeToSquare writes either an 'X' or 'O' to the board.
+    // wrtiteToSquare also writes values to an gameBoard array based on
+    // whether an 'X' or 'O' is played.
+    function writeToBoard(parm) {
+      $('div').one('click', function() {
+        $(this).text(parm);
+        gameBoard[this.id] = Math.random();
+        console.log(gameBoard[this.id]);
+      });
+    }
+    // computerChooses() chooses what computer
+    // function computerChooses() {
 
-    console.log("loaded");
-
+    // }
+    // Game Control
+    // Start game by assigning values to human player
+    writeToBoard("O");
 });
