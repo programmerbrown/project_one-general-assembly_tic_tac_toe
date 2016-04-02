34a289f321082726e3fc4478ccba2c11d8e63df3
Nick Brown <nicholas.g.brown@gmail.com>
2016-04-02 17:13:38 -0400

    Added checkForWinner() function to code

--- js/app.js
+++ js/app.js
@@ -16,6 +16,8 @@ $(document).ready(function() {
     };
 
     var gameCnt = 0;
+    var winningMark = null;
+    var thereIsAWinner = false;
     var gameBoard = [ null, null, null, null, null, null, null, null, null ];
 
     // Functions
@@ -36,15 +38,11 @@ $(document).ready(function() {
       var completed = false;
       var compChoice = null;
       while(!completed) {
-         compChoice = Math.floor(Math.random() * 10);
-        if((gameBoard[compChoice] === null) && (gameCnt < 9)) {
+        compChoice = Math.floor(Math.random() * 10);
+        if(gameBoard[compChoice] === null) {
           $('#' + compChoice).html(computerPlayer["mark"]);
           gameCnt += 1;
           gameBoard[compChoice] = computerPlayer["mark"];
-          // console.log("The gameCnt is " + gameCnt);
-          // console.log("The computer chose square " + compChoice);
-          // console.log("The game board looks like this: ");
-          // console.log(gameBoard);
           completed = true;
         }
       }
@@ -57,14 +55,55 @@ $(document).ready(function() {
     }
 
     // This function will check to see if anyone has won the game.
-    function checkForWinner() {
+    function checkForWinner(board) {
+
+    // Board to cover the scenario of a game winner
+    // Using a switch statement to cover win scenarios for rows
+      if((board[0] == board[1]) && (board[0] == board[2]) && (board[0] !== null)) {            // Check top row
+          thereIsAWinner = true;
+          winningMark = board[0];
+          return thereIsAWinner;
+      } else if((board[3] == board[4]) && (board[3] == board[5]) && (board[3] !== null)) {     // Check middle row
+          thereIsAWinner = true;
+          winningMark = board[3];
+          return thereIsAWinner;
+      } else if((board[6] == board[7]) && (board[6] == board[8]) && (board[6] !== null)) {     // Check bottom row
+          thereIsAWinner = true;
+          winningMark = board[6];
+          return thereIsAWinner;
+      } else if((board[0] == board[3]) && (board[0] == board[6]) && (board[0] !== null)) {     // Check left column
+          thereIsAWinner = true;
+          winningMark = board[0];
+          return thereIsAWinner;
+      } else if((board[1] == board[4]) && (board[1] == board[7]) && (board[1] !== null)) {     // Check middle column
+          thereIsAWinner = true;
+          winningMark = board[1];
+          return thereIsAWinner;
+      } else if((board[2] == board[5]) && (board[2] == board[8]) && (board[2] !== null)) {     // Check right column
+          thereIsAWinner = true;
+          winningMark = board[2];
+          return thereIsAWinner;
+      } else if((board[0] == board[4]) && (board[0] == board[8]) && (board[0] !== null)) {     // Check the first diagonal
+          thereIsAWinner = true;
+          winningMark = board[0];
+          return thereIsAWinner;
+      } else if((board[2] == board[4]) && (board[2] == board[6]) && (board[2] !== null)) {     // Check the second diagonal
+          thereIsAWinner = true;
+          winningMark = board[2];
+          return thereIsAWinner;
+      }
 
     }
+
+
+
     // Game Control
     // Start game by assigning values to human player
     // Initially assign
 
     // writeToBoard(humanPlayer["mark"]);
     // computerChooses();
-
+    // var testGameBoard = [null, 'O', null, null, 'O', null, null, 'O', null ];
+    // console.log(checkForWinner(testGameBoard));
+    // console.log(winningMark);
 });
