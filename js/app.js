262dad5850aae1d7880e95a668df6b16c95dd760
Nick Brown <nicholas.g.brown@gmail.com>
2016-04-02 01:15:21 -0400

    Added functionality to writeToBoard function to also write to gameBoard array while writing ot board

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     var humanPlayer;
     var computerPlayer;
     var gameBoard = [ null, null, null, null, null, null, null, null, null ];
+
     // Functions
     // writeToSquare writes either an 'X' or 'O' to the board.
     // wrtiteToSquare also writes values to an gameBoard array based on
@@ -10,14 +11,15 @@ $(document).ready(function() {
     function writeToBoard(parm) {
       $('div').one('click', function() {
         $(this).text(parm);
-        gameBoard[this.id] = Math.random();
-        console.log(gameBoard[this.id]);
+        gameBoard[this.id] = parm;
       });
     }
     // computerChooses() chooses what computer
     // function computerChooses() {
 
     // }
+
+
     // Game Control
     // Start game by assigning values to human player
     writeToBoard("O");
