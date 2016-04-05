18aab0f920e047604c4c8b4467a872096bda1187
Nick Brown <nicholas.g.brown@gmail.com>
2016-04-05 09:27:12 -0400

    I attempted to add a computer player to the game and this has proven to be too much of a challenge. I'll return to this code at a later date to add a computer player.

--- js/app.js
+++ js/app.js
@@ -5,14 +5,14 @@ $(document).ready(function() {
 
     // This object represents the human player
     var humanPlayer = {
-      mark: null,
-      turn: null
+      mark: 'X',
+      turn: 1
     };
 
     // This object represents the computer player
     var computerPlayer = {
-      mark: null,
-      turn: null
+      mark: 'O',
+      turn: 2
     };
 
     // Defined various parameters needed to monitor the flow of the game
@@ -30,34 +30,37 @@ $(document).ready(function() {
     // setupPlayers() setups up the player objects humanPlayer and computerPlayer
     function setupPlayers() {
       var humanMark = prompt("Enter either X or O to choose which mark you want.");
-      var humanTurn = prompt("Enter either 1 to go first or a 2 to go second.");
+      // var humanTurn = prompt("Enter either 1 to go first or a 2 to go second.");
 
       humanPlayer["mark"] = humanMark.toUpperCase();
       humanMark === 'X' ? computerPlayer["mark"] = 'O' : computerPlayer["mark"] = 'X';
 
-      humanPlayer["turn"] = parseInt(humanTurn);
-      parseInt(humanTurn) === 1 ? computerPlayer["turn"] = 2 : computerPlayer["turn"] = 1;
+      // humanPlayer["turn"] = parseInt(humanTurn);
+      // parseInt(humanTurn) === 1 ? computerPlayer["turn"] = 2 : computerPlayer["turn"] = 1;
     }
 
 
     // humanPlaysMove writes either an 'X' or 'O' to the board.
     // wrtiteToSquare also writes values to an gameBoard array based on
     // whether an 'X' or 'O' is played.
-    function humanPlaysMove(callback) {
+    function humanPlaysMove(thereIsAWinner) {
       // We need to create a pause in how the game flows to allow the human player to
       // choose a square.
+      console.log("Inside humanPlaysMove");
       $('div').one('click', function() {
-        $(this).text(parm);
+        $(this).text(humanPlayer["mark"]);
         gameBoard[this.id] = humanPlayer["mark"];
-        console.log('Game counter is now at ' + moveCounter);
-        callback();
+        moveCounter += 1;
+        console.log('Game counter after human moved is now at ' + moveCounter);
+        thereIsAWinner();
       });
     }
 
     // computerPlaysMove() chooses what position the computer will play
-    function computerPlaysMove(callback) {
+    function computerPlaysMove() {
       var completed = false;
       var compChoice = null;
+      console.log("Inside computerPlaysMoves function.");
       while(!completed) {
         compChoice = Math.floor(Math.random() * 10);
         if(gameBoard[compChoice] === null) {
@@ -65,49 +68,59 @@ $(document).ready(function() {
           gameBoard[compChoice] = computerPlayer["mark"];
           completed = true;
         }
-        callback();
       }
+      console.log("End computerPlaysMoves function.");
+      // callback(humanPlaysMove);
     }
 
     // function playGame() {}
     // This function will check to see if anyone has won the game.
-    function checkForWinner(callback) {
+    function checkForWinner(announceWinner, computerPlaysMove) {
+      console.log("This is the checkForWinner function.");
     // gameBoard to cover the scenario of a game winner
     // Using a switch statement to cover win scenarios for rows
       if((gameBoard[0] == gameBoard[1]) &&  (gameBoard[0] == gameBoard[2]) &&  (gameBoard[0] !== null)) {            // Check top row
           thereIsAWinner = true;
           winningMark = gameBoard[0];
-          return thereIsAWinner;
+          console.log("test");
+          return thereIs;
       } else if((gameBoard[3] == gameBoard[4]) &&  (gameBoard[3] == gameBoard[5]) &&  (gameBoard[3] !== null)) {     // Check middle row
           thereIsAWinner = true;
           winningMark = gameBoard[3];
-          return thereIsAWinner;
+          console.log("test");
+          // return thereIsAWinner;
       } else if((gameBoard[6] == gameBoard[7]) &&  (gameBoard[6] == gameBoard[8]) &&  (gameBoard[6] !== null)) {     // Check bottom row
           thereIsAWinner = true;
           winningMark = gameBoard[6];
-          return thereIsAWinner;
+          console.log("test");
+          // return thereIsAWinner;
       } else if((gameBoard[0] == gameBoard[3]) &&  (gameBoard[0] == gameBoard[6]) &&  (gameBoard[0] !== null)) {     // Check left column
           thereIsAWinner = true;
           winningMark = gameBoard[0];
-          return thereIsAWinner;
+          console.log("test");
+          // return thereIsAWinner;
       } else if((gameBoard[1] == gameBoard[4]) &&  (gameBoard[1] == gameBoard[7]) &&  (gameBoard[1] !== null)) {     // Check middle column
           thereIsAWinner = true;
           winningMark = gameBoard[1];
-          return thereIsAWinner;
+          console.log("test");
+          // return thereIsAWinner;
       } else if((gameBoard[2] == gameBoard[5]) &&  (gameBoard[2] == gameBoard[8]) &&  (gameBoard[2] !== null)) {     // Check right column
           thereIsAWinner = true;
           winningMark = gameBoard[2];
-          return thereIsAWinner;
+          console.log("test");
+          // return thereIsAWinner;
       } else if((gameBoard[0] == gameBoard[4]) &&  (gameBoard[0] == gameBoard[8]) &&  (gameBoard[0] !== null)) {     // Check the first diagonal
           thereIsAWinner = true;
           winningMark = gameBoard[0];
-          return thereIsAWinner;
+          console.log("test");
+          // return thereIsAWinner;
       } else if((gameBoard[2] == gameBoard[4]) &&  (gameBoard[2] == gameBoard[6]) &&  (gameBoard[2] !== null)) {     // Check the second diagonal
           thereIsAWinner = true;
           winningMark = gameBoard[2];
-          return thereIsAWinner;
+          console.log("test");
+          // return thereIsAWinner;
       }
-      callback();
+      return computerPlaysMove();
     }
 
     // Announce winner for the game if one exists
@@ -124,26 +137,28 @@ $(document).ready(function() {
       }
     }
 
-    function playRound(cnt, winCondition, callback){
-      while((cnt < 10) && (winCondition === false)) {      // Keep looping until the moveCounter, or the total number of moves reaches 9
+
+    // function incrementCounter() {
+    //   moveCounter += 1;
+    //   // callback();
+    // }
+
+    function playRound(callback){
+     // Keep looping until the moveCounter, or the total number of moves reaches 9
         // Determine which player should go next
         //
 
-        if(cnt % 2 === 0) {
-          humanPlayer["turn"] === 2 ? humanPlaysMove(callback) : computerPlaysMove(callback);
-          humanPlayer["turn"] === 2 ? console.log("Human plays move.") : console.log("Computer plays move.");
+        if(moveCounter % 2 === 0) {
+          humanPlayer["turn"] === 2 ? humanPlaysMove(checkForWinner()) : computerPlaysMove(checkForWinner);
+          humanPlayer["turn"] === 2 ? console.log("Human plays move. 1") : console.log("Computer plays move.");
         } else {
-          humanPlayer["turn"] === 1 ? humanPlaysMove(humanPlayer["mark"], callback()) : computerPlaysMove(callback());
-          humanPlayer["turn"] === 1 ? console.log("Human plays move.") : console.log("Computer plays move.");
+          humanPlayer["turn"] === 1 ? humanPlaysMove(checkForWinner) : computerPlaysMove(checkForWinner);
+          humanPlayer["turn"] === 1 ? console.log("Human plays move. 2") : console.log("Computer plays move.");
         }
 
-      }
     }
 
-    function incrementCounter(callback) {
-      moveCounter += 1;
-      callback();
-    }
+
 
     // The playGame() function will actually execute the process of playing the game.
     function playGame() {
@@ -154,7 +169,23 @@ $(document).ready(function() {
       // * function setupPlayers() handles all of this
       setupPlayers();
 
-      PlayRound(moveCounter, thereIsAWinner, checkForWinner);
+      while((moveCounter < 10) && (thereIsAWinner === false)) {
+        console.log("moveCounter: " + moveCounter);
+        console.log("Human Player mark: " + humanPlayer["mark"] + " Human Player turn: " + humanPlayer["turn"]);
+        console.log("Computer Player: " + computerPlayer["mark"] + " Computer Player turn: " + computerPlayer["turn"]);
+        // playRound(checkForWinner(incrementCounter));
+        // if(moveCounter % 2 === 0) {
+        //   humanPlayer["turn"] === 2 ? humanPlaysMove(checkForWinner) : computerPlaysMove(checkForWinner);
+        //   humanPlayer["turn"] === 2 ? console.log("Human plays move. 1") : console.log("Computer plays move.");
+        // } else {
+        //   humanPlayer["turn"] === 1 ? humanPlaysMove(checkForWinner) : computerPlaysMove(checkForWinner);
+        //   humanPlayer["turn"] === 1 ? console.log("Human plays move. 2") : console.log("Computer plays move.");
+        // }
+
+        player
+
+        moveCounter += 1;
+      }
 
 
       // moveCounter will be used to keep track of the total number of plays that have happened.
@@ -203,5 +234,8 @@ $(document).ready(function() {
     // setupPlayers();
     // console.log("Human player has " + humanPlayer["mark"] + " mark and turn " + humanPlayer["turn"]);
     // console.log("Computer player has " + computerPlayer["mark"] + " mark and turn " + computerPlayer["turn"]);
-    playGame();
+    // playGame();
+    // humanPlaysMove(computerPlaysMove);
+    var test = "test";
+    humanPlaysMove(checkForWinner);
 });
